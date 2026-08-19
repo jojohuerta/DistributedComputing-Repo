1e0a0bfe1778015fd09630da50b161234115cf0d
jojohuerta <johuerta@itba.edu.ar>
2026-08-19 00:37:37 -0300

    Feat: Lesson 1 - Exercise 4

--- concurrency/src/test/java/ar/edu/itba/pod/concurrency/exercises/e3/ExecutorTest.java
+++ concurrency/src/test/java/ar/edu/itba/pod/concurrency/exercises/e3/ExecutorTest.java
@@ -7,6 +7,11 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class ExecutorTest {
     private GenericService service;
 
@@ -16,7 +21,26 @@ public class ExecutorTest {
     }
 
     @Test
-    public final void test() {
-        assertEquals("No one in queue", "No one in queue");
+    public final void test() throws Exception {
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        Callable<Integer> registerViews = () -> {
+            for (int i = 0; i < 5; i++){
+                service.addVisit();
+            }
+            return service.getVisitCount();
+        };
+
+        //Enviar la tarea al executor y obtener el Future
+        Future<Integer> futureResult = executor.submit(registerViews);
+
+        //Obtener el resultado final del hilo
+        int totalViews = futureResult.get();
+
+        //Hacer la aserción para verificar que sean 5
+        assertEquals(5, totalViews);
+        
+        // Es buena práctica apagar el executor al terminar
+        executor.shutdown();
     }
 }
