10673b2efb43c292c06c2934119b3ca1235bee70
jojohuerta <johuerta@itba.edu.ar>
2026-08-18 23:30:19 -0300

    Feat: Lesson 1 - Exercise 1

--- concurrency/src/main/java/ar/edu/itba/pod/concurrency/exercises/e1/GenericServiceImpl.java
+++ concurrency/src/main/java/ar/edu/itba/pod/concurrency/exercises/e1/GenericServiceImpl.java
@@ -1,42 +1,59 @@
 package ar.edu.itba.pod.concurrency.exercises.e1;
 
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Queue;
+
 /**
  * Basic implementation of {@link GenericService}.
  */
 public  class GenericServiceImpl implements GenericService {
 
+    private int visitCount = 0;
+    private final Queue<String> serviceQueue = new LinkedList<>();
+
     @Override
     public String echo(String message) {
-        return null;
+        return message;
     }
 
     @Override
     public String toUpper(String message) {
-        return null;
+        // if(message == null){
+        //     return null;
+        // }
+        // return message.toUpperCase();
+        return Optional.ofNullable(message).map(String::toUpperCase).orElse(null);
     }
 
     @Override
     public void addVisit() {
-
+        this.visitCount++;
     }
 
     @Override
     public int getVisitCount() {
-        return 0;
+        return this.visitCount;
     }
 
     @Override
     public boolean isServiceQueueEmpty() {
-        return false;
+        return serviceQueue.isEmpty();
     }
 
     @Override
     public void addToServiceQueue(String name) {
-
+        Objects.requireNonNull(name, "El nombre no puede ser null");
+        serviceQueue.add(name);
     }
 
     @Override
     public String getFirstInServiceQueue() {
-        throw new IllegalStateException("No one in queue");
+        if(this.isServiceQueueEmpty()){
+            throw new IllegalStateException("No one in queue");
+        }
+        // .poll() devuelve y elimina el primer elemento de la cola
+        return serviceQueue.poll();
     }
 }
